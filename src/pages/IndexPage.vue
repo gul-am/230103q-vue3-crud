3494b09c66af8b9583cbe37645f39f781fe387f0
Gul AM <gul.amd.git@gmail.com>
2023-01-04 23:33:25 +0200

    chore: working with tables

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,65 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+
+    <q-table
+      title="Treats"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      v-model:pagination="pagination"
+      hide-pagination
+    />
+
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { ref, computed } from 'vue'
 
-export default defineComponent({
-  name: 'IndexPage'
+const columns = [
+  {
+    name: 'desc',
+    required: true,
+    label: 'Dessert (100g serving)',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
+  },
+  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+  { name: 'protein', label: 'Protein (g)', field: 'protein' }
+]
+
+const rows = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0
+  }
+
+]
+
+export default ({
+  setup () {
+    const pagination = ref({
+      sortBy: 'desc',
+      descending: false,
+      page: 2,
+      rowsPerPage: 3
+    })
+
+    return {
+      pagination,
+      columns,
+      rows,
+
+      pagesNumber: computed(() => Math.ceil(rows.length / pagination.value.rowsPerPage))
+    }
+  }
 })
+
 </script>
