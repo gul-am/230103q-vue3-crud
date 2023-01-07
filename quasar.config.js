41446bf5534c15cc0f038201913a0a7effd7d7b3
Gul AM <gul.amd.git@gmail.com>
2023-01-08 00:21:32 +0200

    chore: opening the dev and prod context with new env url configuration

--- quasar.config.js
+++ quasar.config.js
@@ -11,7 +11,7 @@
 const { configure } = require('quasar/wrappers')
 const path = require('path')
 
-module.exports = configure(function (/* ctx */) {
+module.exports = configure(function (ctx) {
   return {
     eslint: {
       // fix: true,
@@ -59,7 +59,13 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16'
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
+      env: {
+        API_URL: ctx.dev
+          ? 'http://localhost:3000/'
+          : 'http://localhost:3000/'
+      },
+
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
